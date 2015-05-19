931f9fe93824b62213e07e0dfee41d7ae2e81956
DanPozza <danpozza21@hotmail.it>
2015-05-19 16:51:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ console.log(stdout);console.log(error);console.log(stderr);
 });
 
 
-var server = app.listen(process.env.PORT||80, function () {
+var server = app.listen(process.env.PORT||3000, function () {
 
   var host = server.address().address;
    port = server.address().port;
