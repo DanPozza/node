7205bf3f53e70ab820d668075521cc82114dc55c
DanPozza <danpozza21@hotmail.it>
2015-06-01 15:33:16 +0200

    Update pc_1.js

--- pc_1.js
+++ pc_1.js
@@ -32,53 +32,7 @@ exec("curl http://192.168.1.108:3000/"+ latlong+'/'+ persone  , function (error,
 console.log(stdout);console.log(error);console.log(stderr);
    	response.send(stdout);
 	console.log(stdout);
-	/*if(stdout == '{"in_fence":true}' )
-		{console.log("son dentro al true");
-	
-var nodemailer = require('nodemailer');
 
-// Create a SMTP transporter object
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'nikotesta@gmail.com',
-        pass: 'nikothebest'
-    },
-	ssl:        true
-});
-
-console.log('SMTP Configured');
-
-// Message object
-var message = {
-
-    // sender info
-    from: 'Nico Testolin <nikotesta@gmail.com>',
-
-    // Comma separated list of recipients
-    to: '<danpozza21@hotmail.it>',
-
-    // Subject of the message
-    subject: 'VIOLAZIONE AREA PROTETTA', //
-
-    // plaintext body
-    text: 'Attenzione è stata violata la zona protetta a latitudine :' + lat+ ' e longitudine : '+long 
-};
-
-console.log('Sending Mail');
-transporter.sendMail(message, function(error, info) {
-    if (error) {
-        console.log('Errore durante invio mail. Disattivare antivirus !! ');
-        console.log(error.message);
-        return;
-    }
-    console.log('Message sent successfully!');
-    console.log('Server responded with "%s"', info.response);
-});
-	
-
-	
-	}*/
 		
 
 });
